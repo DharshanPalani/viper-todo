8aff3f5bb63b5ea9bb4b468283c590f28d5807f3
DharshanPalani <dharshanpalani.2006@gmail.com>
2025-06-13 18:05:35 +0530

    Small change 1

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,14 @@ import "./App.css";
 function App() {
   const [count, setCount] = useState(0);
 
+  function HandleClick() {
+    setCount((count = count + 1));
+  }
+
   return (
     <>
-      <h1>This is Somechanges</h1>
+      <h1>Click the button below</h1>
+      <button onClick={HandleClick}>Button:{count}</button>
     </>
   );
 }
