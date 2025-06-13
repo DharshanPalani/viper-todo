3882569d7d6827cf345c05cf894e0a7c57d141ee
DharshanPalani <dharshanpalani.2006@gmail.com>
2025-06-13 18:13:13 +0530

    FinalChanges

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,17 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
 
   function HandleClick() {
-    setCount((count = count + 1));
+    setCount((count) => count + 1);
   }
 
   return (
     <>
       <h1>Click the button below</h1>
-      <button onClick={HandleClick}>Button:{count}</button>
+      <button onClick={() => HandleClick()}>Button:{count}</button>
     </>
   );
 }
